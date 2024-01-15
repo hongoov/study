522314ea5e2b17222b9d35e2289e2f1db797ecf8
hong <reach9326@naver.com>
2024-01-16 08:41:52 +0900

    var, let, const에 대해 공부하였음.

---
+++ 01_var.js
@@ -0,0 +1,10 @@
+var test1;
+// 결과값 : undefined;
+
+var test2 = 10;
+var test2 = 20;
+// 결과값 : 20;
+
+var test3 = 100;
+test3 = 200;
+// 결과값 : 200;
---
+++ 03_const.js
@@ -0,0 +1,8 @@
+// const test1;
+// 결과값 --> Uncaught SyntaxError: Missing initializer in const declaration
+
+const test2 = 10;
+// const test2 = 20; --> 재선언 불가능
+
+const test3 = 100;
+// const test3 = 200; --> 재할당 불가능
