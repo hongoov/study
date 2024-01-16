915a0b24c2b070745974776ca407265cc6963dab
hong <reach9326@naver.com>
2024-01-16 11:06:05 +0900

    원시 값들의 예시에 대해 공부하였음.

---
+++ 04_primitive.js
@@ -0,0 +1,18 @@
+// string
+const string = "abcdef...";
+const name = "Hong Suk Jin";
+
+// number
+const number = 12345;
+const birth = 19930206;
+
+// boolean
+const booleanA = 100;
+const booleanB = 100;
+const booleanC = 200;
+booleanA === booleanB; // --> output: true;
+booleanA === booleanC; // --> output: false;
+
+// undefined
+let age; // --> output: undefined;
+
