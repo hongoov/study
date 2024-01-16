0deb6a6bc4d10e4f0aa138ad47897345ec05356a
hong <reach9326@naver.com>
2024-01-16 11:01:46 +0900

    변수 선언 방식 var의 예시를 공부하였음.

--- 01_var.js
+++ 01_var.js
@@ -1,10 +1,10 @@
 var test1;
-// 결과값 : undefined;
+// --> output: undefined;
 
 var test2 = 10;
 var test2 = 20;
-// 결과값 : 20;
+// --> output: 20;
 
 var test3 = 100;
 test3 = 200;
-// 결과값 : 200;
+// --> output: 200;
