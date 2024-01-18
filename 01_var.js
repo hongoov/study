5042da4e68adbe7fcdff040d27f4007f1e6d4681
hong <reach9326@naver.com>
2024-01-18 16:42:40 +0900

    변수 선언 방식 var 실습

--- 01_var.js
+++ 01_var.js
@@ -1,10 +1,10 @@
 var test1;
-// --> output: undefined;
+// undefined;
 
 var test2 = 10;
 var test2 = 20;
-// --> output: 20;
+// 20;
 
 var test3 = 100;
 test3 = 200;
-// --> output: 200;
+// 200;
