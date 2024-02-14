f192450b10fc4a83f04e66083de626261df9ecb5
hong <reach9326@naver.com>
2024-02-14 11:54:01 +0900

    h1, form, fieldset 태그를 innerHTML로 추가하기 위해 삭제함

--- string_02/script.js
+++ string_02/script.js
@@ -1,13 +1,4 @@
 const root = document.getElementById('root');
-const h1 = document.createElement('h1');
-const form = document.createElement('form');
-const fieldset = document.createElement('fieldset');
-
-h1.textContent = 'String 연습하기';
-
-root.appendChild(h1);
-root.appendChild(form);
-form.appendChild(fieldset);
 
 const elements = [
 	{num: 1, label: '문자열 ', placeholder: '문자열을 입력하고 ENTER를 누르세요.', subtitle: '2번째 문자는?'},
@@ -16,6 +7,7 @@ const elements = [
 	{num: 4, label: '영어를 ', placeholder: '길이를 반환할 문자열을 입력하세요,', subtitle: '길이는?'},
 	{num: 5, label: '영어를 ', subtitle: 'A를 모두 a로 바꾸면?'},
 ];
+// 배열인데 굳이 숫자를 부여한...? --> 불필요한 프로퍼티. num이라는 명칭도 의미가 강하지 않다. label도 좀
 
 elements.forEach((element) => {
     const stringExercise = `
@@ -28,18 +20,22 @@ elements.forEach((element) => {
             <h4 class="result"></h4>
         </section>
     `;
+	// element.num === 5 --> 이 부분
 
     fieldset.innerHTML += stringExercise;
+	// 5번의 재할당이 일어나는데, 한 번에 관리하는 방법은 없을까? --> 불필요한 할당이 4번 발생
+	// innerHTML을 한 번만 하고
 });
 
 const result = Array.from(document.querySelectorAll('.result'));
+// results
 
 const exs = [
 	(str) => str.charAt(1),
 	() => ['CP song', 'EG Lee', 'CP Hong', 'CP Kim', 'HR Long'].filter(item => item.startsWith('CP')),
 	(str) => str.toLowerCase(),
 	(str) => str.length,
-	() => 'A B C D A A B N D D C C E E'.toLowerCase().replace(/A/g),
+	() => 'A B C D A A B N D D C C E E'.toLowerCase().replace(/A/g), // --> 오류 수정
 ];
 
 form.addEventListener('change', (e) => {
@@ -47,5 +43,10 @@ form.addEventListener('change', (e) => {
 
 	result.forEach((item, index) => {
 		item.textContent = exs[index](index === 0 ? str1Input.value : (index === 2 ? str3Input.value : (index === 3 ? str4Input.value : '')));
-	}); 
+	});
+	// item 말고 result
 });
+
+// ?? 의미, join
+// change 할 때마다 무언가 일어남
+// 하나 바꾸고 하나 commit
